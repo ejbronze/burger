ad8a113d99d2388c26e8c815d683aa692aa839e0
Eddie Jaquez <erjaquez@gmail.com>
2019-04-23 18:58:50 -0400

    connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,25 @@
-// Connect Node to MySQL.
 var mysql = require("mysql");
 
+var connection;
+
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: '',
-        database: "burgers_db"
-    });
-};
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-// Export the connection.
 module.exports = connection;
